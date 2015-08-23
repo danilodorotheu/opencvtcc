7673428d56187dff33f463c60cfbc08156b57721
Danilo Dorotheu <danilo.dorotheu@live.com>
2015-08-23 00:40:49 -0300

    Teste de Repositorio

--- src/main/java/br/com/opencv/main/Main.java
+++ src/main/java/br/com/opencv/main/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
